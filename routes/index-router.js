0c5978fe7f88e364419ada6c1bf6632b8cd0ccaf
Bn <bnjmn.kmc+github@gmail.com>
2024-08-19 14:41:51 +0800

    refactor index.ejs

--- routes/index-router.js
+++ routes/index-router.js
@@ -3,23 +3,37 @@ const express = require("express");
 const router = express.Router();
 
 /**
- * Useful notes for reference throughout implementation:
+ * Useful notes to reference throughout implementation!
  *
+ * ===== =====
+ * Express functions:
+ * ----- -----
  * app.get()/router.get() = the endpoint with prefix, if any
  * response.render()      = looks into 'views' dir for a matching file name to load
  * response.redirect()    = the endpoint without prefix
  *
+ * ===== =====
+ * Forms and accessibility:
+ * ----- -----
  * <* name="someName"> is used as variable name for routing in .js
  *
  * <form action="endpointHere"> must match with .js .post("endpointHere") function
  * <label for="matchingName"> selects <input id="matchingName">, this helps with accessibility
  *
  * <button name="whatIsYourName"> returns its <button value="theName">
- *
+ 
+ * ===== =====
+ * Linking URLs and source files:
+ * ----- -----
  * <a href=""> = the endpoint with prefix, if any
  * <a href=""> only does GET requests
  *
- * <* href=""> looks into 'public' dir for a matching file name to load
+ * Whenever a source file is referenced (eg. css' <link href=""> / url() / <img src=""> / <script src="">),
+ * Express looks into 'public' dir for a matching file name to load
+ * 
+ * This is set in index.js via "express.static()"
+ * 
+ * ===== =====
  */
 
 // Home
