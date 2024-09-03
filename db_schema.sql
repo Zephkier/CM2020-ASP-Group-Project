339196dbed490695f7f66bd49f86dcd53854dca0
Bn <bnjmn.kmc+github@gmail.com>
2024-09-03 23:29:12 +0800

    feat: add varying number of topics for every course

--- db_schema.sql
+++ db_schema.sql
@@ -85,14 +85,54 @@ CREATE TABLE topics (
     FOREIGN KEY (course_id) REFERENCES courses(id)
 );
 
+-- HTML course
+INSERT INTO topics (course_id, name, description, video_url) VALUES (1, 'Introduction to HTML', 'Learn the basics of HTML', 'https://www.youtube.com/watch?v=dD2EISBDjWM');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (1, 'First Web Page', 'Let''s build our first web page', 'https://www.youtube.com/watch?v=-USAeFpVf_A');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (1, 'Useful HTML Tips', 'We''ll be learning about line breaks, spacing and so on', 'https://www.youtube.com/watch?v=i3GE-toQg-o');
+
+-- CSS course
+INSERT INTO topics (course_id, name, description, video_url) VALUES (2, 'Introduction to CSS', 'Learn the basics of CSS', 'https://www.youtube.com/watch?v=qKoajPPWpmo');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (2, 'Changing Basics', 'Learn how to change fonts and its properties', 'https://www.youtube.com/watch?v=UO0ZPL8yMpU');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (2, 'Selectors', 'All about CSS selectors', 'https://www.youtube.com/watch?v=JT0gyzbpD2U');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (2, 'Header and Borders', 'Useful tips and tricks for eyeballing it', 'https://www.youtube.com/watch?v=hCoMjvtsyPA');
+
+-- C Sharp course
+INSERT INTO topics (course_id, name, description, video_url) VALUES (3, 'Introduction to C#', '100 seconds of C#', 'https://www.youtube.com/watch?v=ravLFzIguCM');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (3, 'Getters and Setters', 'What do you get and what do you set', 'https://www.youtube.com/watch?v=8FmE_-QXg3Y');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (3, 'Polymorphism', 'No, this isn''t Yu-Gi-Oh polymerisation', 'https://www.youtube.com/watch?v=nYCMW3kfTvs');
+
+-- Java course
+INSERT INTO topics (course_id, name, description, video_url) VALUES (4, 'Introduction to Java', '100 seconds of Java', 'https://www.youtube.com/watch?v=l9AzO1FMgM8');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (4, 'Java Haters', 'All about Java but for the haters', 'https://www.youtube.com/watch?v=m4-HM_sCvtQ');
+
 -- C++ course
-INSERT INTO topics (course_id, name, description, video_url) VALUES (5, 'Introduction', 'Learn the basics of C++', 'https://www.youtube.com/watch?v=MNeX4EGtR5Y');
-INSERT INTO topics (course_id, name, description, video_url) VALUES (5, 'Variables', 'Learn how to use variables in C++', 'https://www.youtube.com/watch?v=zB9RI8_wExo');
-INSERT INTO topics (course_id, name, description, video_url) VALUES (5, 'Functions', 'Learn how to use functions in C++', 'https://www.youtube.com/watch?v=6SnxFx9aRps');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (5, 'Introduction', '100 seconds of C++', 'https://www.youtube.com/watch?v=MNeX4EGtR5Y');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (5, 'Variables and Data Types', 'Learn the fundamentals of this language', 'https://www.youtube.com/watch?v=4psGUiKacPQ');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (5, 'What is ''const''', 'One of the more important data types', 'https://www.youtube.com/watch?v=MwQEaCsS6UM');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (5, 'Namespaces', 'Organising your code just got easier', 'https://www.youtube.com/watch?v=2lcIKzFHjSM');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (5, 'Typedef and Type Aliases', 'Sort of like nicknames but not for bullying', 'https://www.youtube.com/watch?v=7TJ7Z1-V_24');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (5, 'User Input', 'Make it interactable', 'https://www.youtube.com/watch?v=imiIhu9u670');
+
+-- Unreal Engine course
+INSERT INTO topics (course_id, name, description, video_url) VALUES (6, 'Welcome to Unreal', 'This is pretty unreal', 'https://www.youtube.com/watch?v=DXDe-2BC4cE');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (6, 'Installing and Setup', 'Let''s get on the same page before proceeding', 'https://www.youtube.com/watch?v=bDUFB1ng00Q');
+
+-- Python course
+INSERT INTO topics (course_id, name, description, video_url) VALUES (7, 'Data Structures', 'This is how everyone gets started in Python', 'https://www.youtube.com/watch?v=gOMW_n2-2Mw');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (7, 'Recursion', 'Like echos, but in Python', 'https://www.youtube.com/watch?v=ivl5-snqul8');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (7, 'Speed Comparison', 'Don''t listen to the Python haters', 'https://www.youtube.com/watch?v=VioxsWYzoJk');
+
+-- UI-UX Design course
+INSERT INTO topics (course_id, name, description, video_url) VALUES (9, 'World''s Shortest Design Course', 'Yep, what the title said', 'https://www.youtube.com/watch?v=wIuVvCuiJhU');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (9, 'Choosing Colours', 'Imagine not being able to read this', 'https://www.youtube.com/watch?v=HAlIWRcldoc');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (9, 'Dark Mode', 'My favourite setting', 'https://www.youtube.com/watch?v=6U9iC-c15AI');
 
 -- Adobe Illustrator course
-INSERT INTO topics (course_id, name, description, video_url) VALUES (10, 'Introduction', 'Learn the basics of Adobe Illustrator', 'https://www.youtube.com/watch?v=QKWnkIPur2Q');
-INSERT INTO topics (course_id, name, description, video_url) VALUES (10, 'Color Modes', 'Learn how to use color modes in Adobe Illustrator', 'https://www.youtube.com/watch?v=H0Bkk9UMkVc');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (10, 'Orange', 'Orange', 'https://www.youtube.com/watch?v=Gv9QH0Z-GMU');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (10, 'The Most Overpowered Tool', 'They say the pen is mightier than the sword', 'https://www.youtube.com/watch?v=Rk-JGsriJ4o');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (10, 'Colouring Tutorial', 'Yes, this is important', 'https://www.youtube.com/watch?v=eoeYMSdh0G8');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (10, 'Sketch to Vector', 'Turn your drawings immortal', 'https://www.youtube.com/watch?v=HeLb0dy81Lc');
+INSERT INTO topics (course_id, name, description, video_url) VALUES (10, '2D to 3D', 'I wonder when 4D will come around', 'https://www.youtube.com/watch?v=-ZoDHEsd0Y4');
 
 CREATE TABLE enrollments (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
